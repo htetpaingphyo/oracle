06fc5ccb9bca9f33d5dedcf39b013200c2389c48
Htet Paing <htetpp@ami-insurance.com>
2018-03-20 17:08:01 +0630

    Renaming and modifying files for dir-hierarchy.

--- custom reports/fire_perils_data.sql
+++ custom reports/fire_perils_data.sql
@@ -1,18 +1,30 @@
 exec pk_stat_reports.pr_del_x_data;
 
-exec pk_stat_reports.pr_pop_x_data_rc_date('01-NOV-2017', '30-NOV-2017');
+exec pk_stat_reports.pr_pop_x_data_rc_date('01-OCT-2017', '31-OCT-2017');
+
+select * from fire_perils_report;
 
 -- // TEST // --
-create or replace view fire_perils_report as select 
-    distinct x.pol_policy_no "POLICY NO", loc_province "STAGE", loc_district "DISTRICT", loc_postal_code "TOWNSHIP", loc_cyclone "VILLAGE_TRACK", loc_earthqk "VILLAGE",
-    (select listagg(cus_name, ', ') within group (order by pol_policy_no) 
+create or replace view fire_perils_report as 
+select 
+    distinct x.pol_policy_no "POLICY_NO", 
+    loc_province "STATE", 
+    loc_district "DISTRICT", 
+    loc_postal_code "TOWNSHIP", 
+    loc_cyclone "VILLAGE_TRACK", 
+    loc_earthqk "VILLAGE", 
+    (select listagg(cus_name, ', ') within group (order by pol_policy_no)  
         from pol_data 
         where pol_policy_no=x.pol_policy_no 
-        and pol_seq_no=x.pol_seq_no) "NAME",
+        and pol_seq_no=x.pol_seq_no) "CUS_NAME",  
     (select listagg(pol_transaction_type, ', ') within group (order by pol_policy_no) 
         from pol_data 
         where pol_policy_no=x.pol_policy_no 
-        and pol_seq_no=x.pol_seq_no) "POLICY TYPE", 
+        and pol_seq_no=x.pol_seq_no) "POLICY_TYPE", 
+    x.pol_period_from "PERIOD_FROM", 
+    x.pol_period_to "PERIOD_TO", 
+    x.pol_trans_effect_date "EFFECT_DATE", 
+    x.prs_name "RISK_NAME",      
     ( select listagg(ppr_premium, ', ') within group (order by pol_policy_no) 
         from pol_risk_perils 
         where pol_policy_no = x.pol_policy_no 
@@ -34,45 +46,50 @@ create or replace view fire_perils_report as select
         where pol_policy_no = x.pol_policy_no 
         and pol_seq_no = x.pol_seq_no 
         and prs_name = x.prs_name 
-        and prl_description='RIOT STRIKE AND MALICIOUS DAMAGE' ) "RSM",
+        and prl_description='RIOT STRIKE AND MALICIOUS DAMAGE' ) "RSM", 
     ( select listagg(ppr_premium, ', ') within group (order by pol_policy_no) 
         from pol_risk_perils 
         where pol_policy_no = x.pol_policy_no 
         and pol_seq_no = x.pol_seq_no 
         and prs_name = x.prs_name 
-        and prl_description='EARTH-QUAKE FIRE, FIRE AND SHOCK DMG CAUSED BY EQ' ) "EQ",  
-    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='IMPACT DAMAGE' ) "ID",
-    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='SPONTANEOUS COMBUSTION' ) "SP",
-    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='FLOOD AND INUNDATION' ) "FLOOD",
+        and prl_description='EARTH-QUAKE FIRE, FIRE AND SHOCK DMG CAUSED BY EQ' ) "EQ", 
+    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='IMPACT DAMAGE' ) "ID", 
+    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='SPONTANEOUS COMBUSTION' ) "SP", 
+    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='FLOOD AND INUNDATION' ) "FLOOD", 
     ( select listagg(ppr_premium, ', ') within group (order by ppr_premium) 
         from pol_risk_perils 
         where pol_policy_no = x.pol_policy_no 
         and pol_seq_no = x.pol_seq_no 
         and prs_name = x.prs_name 
-        and prl_description='BURGLARY' ) "BUR",
-    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='SUBSIDENCE & LANDSLIDE' ) "SL", 
+        and prl_description='BURGLARY' ) "BUR", 
+    ( select ppr_premium from pol_risk_perils where pol_policy_no = x.pol_policy_no and pol_seq_no = x.pol_seq_no and prs_name = x.prs_name and prl_description='SUBSIDENCE & LANDSLIDE' ) "SL",  
+    x.ppr_sum_insured "SUM_INSURED",  
+    ( select sum(ppr_premium) from pol_risk_perils where pol_policy_no=x.pol_policy_no and prs_name=x.prs_name group by prs_name) "RISK_PREMIUM", 
     ( select listagg(pol_transaction_amount, ', ') within group (order by pol_policy_no) 
         from pol_data 
         where pol_policy_no=x.pol_policy_no 
-        and pol_seq_no=x.pol_seq_no) "BASIC PREMIUM",
-    ( select listagg(pol_total_transaction_amount, ', ') within group (order by pol_policy_no) 
+        and pol_seq_no=x.pol_seq_no) "NET_PREMIUM", 
+    ( select listagg(pol_total_transaction_amount, ', ') within group (order by pol_policy_no)   
         from pol_data 
         where pol_policy_no=x.pol_policy_no 
-        and pol_seq_no=x.pol_seq_no) "TOTAL PREMIUM",
+        and pol_seq_no=x.pol_seq_no) "TOTAL_PREMIUM", 
     ( select listagg (info_value, ', ') within group (order by pol_policy_no) 
         from pol_risk_info 
         where pol_policy_no=x.pol_policy_no 
         and prs_name=x.prs_name 
-        and pin_description='CLASS OF THE BUILDING'
-    ) "BUILDING CLASS",
+        and pin_description='CLASS OF THE BUILDING' 
+    ) "BUILDING CLASS", 
     ( select listagg (info_value, ', ') within group (order by pol_policy_no) 
         from pol_risk_info 
         where pol_policy_no=x.pol_policy_no 
         and prs_name=x.prs_name 
-        and pin_description='OCCUPATION OF THE BUILDING'
+        and pin_description='OCCUPATION OF THE BUILDING' 
     ) "BUILDING OCCUPATION", 
     ( select listagg(commission_amt, ', ') within group (order by pol_policy_no) 
         from pol_data 
         where pol_policy_no=x.pol_policy_no 
-        and pol_seq_no=x.pol_seq_no) "COMMISSION"
-from pol_risk_perils x, pol_data_locations y where x.pol_policy_no=y.pol_policy_no and pol_prd_code in ('FFI', 'FCS', 'FSD');
+        and pol_seq_no=x.pol_seq_no) "COMMISSION" 
+from pol_risk_perils x, pol_data_locations y 
+where x.pol_policy_no=y.pol_policy_no 
+and pol_prd_code in ('FFI') /*, 'FCS', 'FSD'*/ 
+order by 1;
